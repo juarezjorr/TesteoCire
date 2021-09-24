b9e5359b873386c138949d681bb631e479cd2c63
juarezjorr <juarezjorr@gmail.com>
2021-09-24 18:50:20 -0500

    repositorio tercero

--- RESP/app_resp/views/Start/Start.js
+++
@@ -1,8 +0,0 @@
-var seccion = '';
-
-$(document).ready(function () {
-	console.log('start');
-	pos = 4;
-	verifica_usuario();
-	// busca_sidebar();
-});
--- RESP/app_resp/views/TAdmin/TAdmin.js
+++
@@ -1,8 +0,0 @@
-var seccion = '';
-$(document).ready(function () {
-	pos = 4;
-	verifica_usuario();
-	busca_sidebar();
-});
-
-function busca_sidebar() {}
